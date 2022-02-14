97548bd64c9ef10ed46c8de0841bd1ba86882f1a
Ale De Alvino <alejandra.dealvino.melgoza@gmail.com>
2022-02-14 01:07:00 -0600

    Primer commit

---
+++ cambio.js
@@ -0,0 +1,11 @@
+function perfil_Ale(){
+    window.location="perfil.html";
+}
+
+function perfil_Abi(){
+    window.location="perfil.html";
+}
+
+function perfil_Enrique(){
+    window.location="perfil.html";
+}
